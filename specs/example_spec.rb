694d6be6a0187562cf210bf66422a5ae72ecdf76
David Shin <xdavidshinx@gmail.com>
2017-04-20 14:57:20 -0700

    Update and rename coin_combinations_spec.rb to example_spec.rb

--- specs/example_spec.rb
+++ specs/example_spec.rb
@@ -3,6 +3,6 @@ require('rspec')
 
 describe('String#rock_paper_scissors') do
   it("returns the result for a rock, paper, scissors game") do
-  expect("Rock".rps().to(eq("Computer threw Rock. Draw Game")))
+  expect("Rock".rps()).to(eq("Computer threw Rock. Draw Game"))
   end
 end
